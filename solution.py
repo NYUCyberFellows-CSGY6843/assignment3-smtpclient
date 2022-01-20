e7e81b82ada3dbc015d92a7a59a21bbfed2efbbd
Rockstaru <slupeasaurus@gmail.com>
2022-01-19 19:34:10 -0500

    Initial commit

---
+++ solution.py
@@ -0,0 +1,54 @@
+from socket import *
+
+
+def smtp_client(port=1025, mailserver='127.0.0.1'):
+    msg = "\r\n My message"
+    endmsg = "\r\n.\r\n"
+
+    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
+
+    # Create socket called clientSocket and establish a TCP connection with mailserver and port
+
+    # Fill in start
+    # Fill in end
+
+    recv = clientSocket.recv(1024).decode()
+    #print(recv) #You can use these print statement to validate return codes from the server.
+    #if recv[:3] != '220':
+    #    print('220 reply not received from server.')
+
+    # Send HELO command and print server response.
+    heloCommand = 'HELO Alice\r\n'
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
+    #print(recv1) 
+    #if recv1[:3] != '250':
+    #    print('250 reply not received from server.')
+
+    # Send MAIL FROM command and handle server response.
+    # Fill in start
+    # Fill in end
+
+    # Send RCPT TO command and handle server response.
+    # Fill in start
+    # Fill in end
+
+    # Send DATA command and handle server response.
+    # Fill in start
+    # Fill in end
+
+    # Send message data.
+    # Fill in start
+    # Fill in end
+
+    # Message ends with a single period, send message end and handle server response.
+    # Fill in start
+    # Fill in end
+
+    # Send QUIT command and handle server response.
+    # Fill in start
+    # Fill in end
+
+
+if __name__ == '__main__':
+    smtp_client(1025, '127.0.0.1')
