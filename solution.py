0ca3aa20d1d41756d6b15c634a9f570541ae533a
OHarilal <78577011+OHarilal@users.noreply.github.com>
2022-04-22 19:48:43 -0400

    4/22

--- solution.py
+++ solution.py
@@ -6,9 +6,11 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-
+    mailServer = "smtp.google.com"
+    serverPort = 25
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
-
+    clientSocket = socket(AF_INET,SOCK_STREAM)
+    clientSocket.connect((mailServer,serverPort))
     # Fill in start
     # Fill in end
 
@@ -27,14 +29,27 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
+    mailFromCommand = "MAIL FROM: <MY EMAIL ADDRESS>\r\n"
+    clientSocket.send(mailFromCommand)
+
+    recv1 = clientSocket.recv(1024)
+    print(recv1)
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
+    rcptToCommand = "RCPT TO: <MY EMAIL ADDRESS>\r\n"
+    clientSocket.send(rcptToCommand)
+
+    recv1 = clientSocket.recv(1024)
+    print(recv1)
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
+    clientSocket.send("DATA\r\n")
+    recv1 = clientSocket.recv(1024)
+    print(recv1)
     # Fill in end
 
     # Send message data.
@@ -43,10 +58,14 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
+    clientSocket.send(msg,endmsg)
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
+    clientSocket.send("QUIT")
+
+    recv1 = clientSocket.recv(1024)
     # Fill in end
 
 
