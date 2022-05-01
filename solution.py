7900a87f6b5b86a3a77ffa44c16a258b73f8ac17
OHarilal <78577011+OHarilal@users.noreply.github.com>
2022-05-01 11:01:14 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,11 +6,10 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    mailServer = "smtp.google.com"
-    serverPort = 25
+    mailServer = "smtp.gmail.com"
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET,SOCK_STREAM)
-    clientSocket.connect((mailServer,serverPort))
+    clientSocket.connect((mailServer,port))
     # Fill in start
     # Fill in end
 
@@ -20,8 +19,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     #    print('220 reply not received from server.')
 
     # Send HELO command and print server response.
-    heloCommand = 'HELO Alice\r\n'
-    clientSocket.send(heloCommand.encode())
+    HELOCommand = "HELO Alice\r\n"
+    clientSocket.send(HELOCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
     #print(recv1) 
     #if recv1[:3] != '250':
@@ -29,40 +28,44 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    MAILFROM = "MAIL FROM: <MY EMAIL ADDRESS>\r\n"
-    clientSocket.send(MAILFROM)
+    MAILFROMCommand = "MAIL FROM: <MY EMAIL ADDRESS>\r\n"
+    clientSocket.send(MAILFROMCommand)
     recv1 = clientSocket.recv(1024)
     #print(recv1)
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
-    RCPTTO = "RCPT TO: <MY EMAIL ADDRESS>\r\n"
-    clientSocket.send(RCPTTO)
+    RCPTTOCommand = "RCPT TO: <MY EMAIL ADDRESS>\r\n"
+    clientSocket.send(RCPTTOCommand)
     recv1 = clientSocket.recv(1024)
     #print(recv1)
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
-    clientSocket.send("DATA\r\n")
+    DATACommand = "DATA\r\n"
+    clientSocket.send(DATACommand)
     recv1 = clientSocket.recv(1024)
     #print(recv1)
     # Fill in end
 
     # Send message data.
     # Fill in start
+    clientSocket.send(msg)
+    recv1 = clientSocket.recv(1024)
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
     clientSocket.send(msg,endmsg)
+    recv1 = clientSocket.recv(1024)
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
-    clientSocket.send("QUIT")
-
+    QUITCommand = "QUIT"
+    clientSocket.send(QUITCommand)
     recv1 = clientSocket.recv(1024)
     # Fill in end
 
